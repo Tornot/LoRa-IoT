09376baf5c6e0495564e6cbfcf3cfc9408c52bfa
Arnaud Torfs <10039@etu.he2b.be>
2020-11-02 14:19:33 +0100

    Decoding Hexa to Binary + Clean

--- Codes Serveur/WebApp/main.py
+++ Codes Serveur/WebApp/main.py
@@ -4,32 +4,6 @@ import binascii
 import time
 from ast import literal_eval
 
-# float_str = "-0b101010101"
-# result = float(literal_eval(float_str))
-from codecs import decode
-import struct
-
-
-def bin_to_float(b):
-    """ Convert binary string to a float. """
-    bf = int_to_bytes(int(b, 2), 8)  # 8 bytes needed for IEEE 754 binary64.
-    return struct.unpack('>d', bf)[0]
-
-
-def int_to_bytes(n, length):  # Helper function
-    """ Int/long to byte string.
-
-        Python 3.2+ has a built-in int.to_bytes() method that could be used
-        instead, but the following works in earlier versions including 2.x.
-    """
-    return decode('%%0%dx' % (length << 1) % n, 'hex')[-length:]
-
-
-def float_to_bin(value):  # For testing.
-    """ Convert float to 64-bit binary string. """
-    [d] = struct.unpack(">Q", struct.pack(">d", value))
-    return '{:064b}'.format(d)
-
 
 def get_binary(string):
     out_string = ""
@@ -47,19 +21,6 @@ def message_received_callback(msg, client):
     string = msg.payload_fields[0]
     binary = get_binary(string)
     print(binary)
-
-    # my_float = bin_to_float(binary)
-    # print(my_float)
-    # message_type = MessageType.Temperature
-    # if message_type == MessageType.Temperature:
-    #     _temperature = 215
-    #     timestamp = time.time()
-    #     database.add_temperature_measure_to_database(timestamp, _temperature, client)
-    #
-    # elif message_type == MessageType.Luminosity:
-    #     luminosity = 215
-    #     timestamp = time.time()
-    #     database.add_luminosity_measure_to_database(timestamp, luminosity, client)
 
 
 database = dbUtil.Database()
